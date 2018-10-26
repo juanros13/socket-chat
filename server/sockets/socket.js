be341d8fe3f34b262d57740d281453fde8042e3b
Juan Roberto Rosales Vargas <juanros13@gmail.com>
2018-10-26 18:17:45 -0500

    Primer commir

---
+++ server/sockets/socket.js
@@ -0,0 +1,53 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/Usuarios');
+const { crearMensaje } = require('../utilidades/utilidades')
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    console.log('Usuario conectado');
+
+    client.on('entrarChat', (data, callback) => {
+        // console.log(data);
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            })
+        }
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonaPorSala(data.sala));
+        callback(usuarios.getPersonaPorSala(data.sala));
+    });
+
+    client.on('crearMensaje', (data) => {
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje)
+    });
+
+
+    client.on('disconnect', () => {
+        let personaEliminada = usuarios.borrarPersona(client.id);
+        console.log('Usuario desconectado', personaEliminada);
+
+        client.broadcast.to(personaEliminada.sala).emit('crearMensaje',
+            crearMensaje('Admin', `${ personaEliminada.nombre } ha dejado el chat`)
+        );
+        /* client.broadcast.emit('crearMensaje', {
+            usuario: 'Administrador',
+            mensaje: `${ personaEliminada.nombre } ha dejado el chat`
+        }); */
+        client.broadcast.to(personaEliminada.sala).emit('listaPersonas', usuarios.getPersonaPorSala(personaEliminada.sala));
+
+    });
+    client.on('mensajePrivado', data => {
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mesaje));
+    });
+
+
+});
